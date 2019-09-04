6c8159d2ae6823df094eb4692a12796965dd798d
SpartanD39 <admin@spartand39.net>
2019-09-04 01:40:15 -0600

    improved controls for post editing/deletion. now only allows edit/deletion of posts belonging to the logged-in user. super-admin roles can edit/delete all posts.

--- admin/admin-post.php
+++ admin/admin-post.php
@@ -78,15 +78,20 @@ if(isset($_GET['action']) && !empty($_GET['action'])) {
             <td>{$post["post_author"]}</td>
             <td>{$post["post_title"]}</td>
             <td>{$post["cat_name"]}</td>
-			<td>{$post["post_tags"]}</td>
-			<td><img src="../uploads/images/{$post["post_image"]}" alt="" class="img-thumbnail" style="height: auto; width: auto; max-height: 150px; max-width: 150px;"></td>
-			<td>{$post["post_status"]}</td>
-			<td>{$post["post_date"]}</td>
+			      <td>{$post["post_tags"]}</td>
+			      <td><img src="../uploads/images/{$post["post_image"]}" alt="" class="img-thumbnail" style="height: auto; width: auto; max-height: 150px; max-width: 150px;"></td>
+			      <td>{$post["post_status"]}</td>
+			      <td>{$post["post_date"]}</td>
             <td><a href="../index.php?id={$post["post_id"]}&view=post" target="_blank">View</a></td>
-            <td><a href="admin-post.php?id={$post["post_id"]}&action=edit">Edit</a></td>
-            <td><a href="admin-post.php?id={$post["post_id"]}&action=delete">Delete</a></td>
-        </tr>
 EOT;
+            if($_SESSION["user_id"] == $post["post_author_id"] || $_SESSION["user_role"] == "super-admin") {
+              echo "<td><a href=\"admin-post.php?id={$post["post_id"]}&action=edit\">Edit</a></td>";
+              echo "<td><a href=\"admin-post.php?id={$post["post_id"]}&action=delete\">Delete</a></td>";
+            } else {
+              echo "<td></td>";
+              echo "<td></td>";
+            }
+            echo "</tr>";
     }
 }
 ?>
